cbc72e62afd95d0723678261e41af1f780e6708e
Juan David <juandavid918@gmail.com>
2023-06-04 19:32:15 -0500

    Mostrando Topbar distinto cuando se ingresa a un problema

---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,11 @@
+import Topbar from "@/components/Topbar/Topbar";
+
+const ProblemPage = () => {
+    return (
+        <div>
+            <Topbar problemPage={ true } />
+        </div>
+    );
+}
+
+export default ProblemPage;
