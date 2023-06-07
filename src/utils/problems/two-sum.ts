1af5d59010e71b11c9ad10e05b24853df5beda0f
Juan David <juandavid918@gmail.com>
2023-06-06 19:55:39 -0500

    Reestructurados los tipos

--- src/utils/problems/two-sum.ts
+++ src/utils/problems/two-sum.ts
@@ -1,24 +1,5 @@
+import { Problem } from "../types/problem";
 import assert from "assert";
-
-export type Example = {
-    id: number,
-    inputText: string,
-    outputText: string,
-    explanation?: string,
-    img?: string
-};
-
-export type Problem = {
-	id: string,
-	title: string,
-	problemStatement: string,
-    examples: Example[],
-    constraints: string,
-    order: number,
-    starterCode: string,
-    handlerFunction: ((fn: any) => boolean) | string,
-    starterFunctionName: string
-};
 
 const starterCodeTwoSum = `function twoSum(nums, target) {
     // Write your code here
