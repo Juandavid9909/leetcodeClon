9cc033f964c0ac2d3d5acbe00f002cbe676b964f
Juan David <juandavid918@gmail.com>
2023-06-07 20:38:40 -0500

    Arreglado problema de hydration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
+import useHasMounted from "@/hooks/useHasMounted";
 
 function Home() {
   const [loadingProblems, setLoadingProblems] = useState(true);
+  const hasMounted = useHasMounted();
+
+  if(!hasMounted) {
+    return null;
+  }
 
   return (
     <>
