9f5cc94d8819e7232e75bd5c2292c3113a3bc59d
Juan David <juandavid918@gmail.com>
2023-06-07 21:29:19 -0500

    Pantalla completa y salir de la misma

--- src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -1,6 +1,38 @@
-import { AiOutlineFullscreen, AiOutlineSetting } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { AiOutlineFullscreen, AiOutlineFullscreenExit, AiOutlineSetting } from "react-icons/ai";
 
 const PreferenceNav = () => {
+	const [isFullScreen, setIsFullScreen] = useState(false);
+
+	const handleFullScreen = () => {
+		if(isFullScreen) {
+			document.exitFullscreen();
+		}
+		else {
+			document.documentElement.requestFullscreen();
+		}
+
+		setIsFullScreen(!isFullScreen);
+	}
+
+	useEffect(() => {
+		function exitHandler(e: any) {
+			if(!document.fullscreenElement) {
+				setIsFullScreen(false);
+				return;
+			}
+
+			setIsFullScreen(true);
+		}
+
+		if(document.addEventListener) {
+			document.addEventListener("fullscreenchange", exitHandler);
+			document.addEventListener("webkitfullscreenchange", exitHandler);
+			document.addEventListener("mozfullscreenchange", exitHandler);
+			document.addEventListener("MSFullscreenChange", exitHandler);
+		}
+	}, [isFullScreen]);
+
     return (
         <div className="flex items-center justify-between bg-dark-layer-2 h-11 w-full">
 			<div className="flex items-center text-white">
@@ -20,9 +52,12 @@ const PreferenceNav = () => {
 					<div className="preferenceBtn-tooltip">Settings</div>
 				</button>
 
-				<button className="preferenceBtn group">
+				<button
+					className="preferenceBtn group"
+					onClick={ handleFullScreen }
+				>
 					<div className="h-4 w-4 text-dark-gray-6 font-bold text-lg">
-						<AiOutlineFullscreen />
+						{ !isFullScreen ? <AiOutlineFullscreen /> : <AiOutlineFullscreenExit /> }
 					</div>
                     
 					<div className="preferenceBtn-tooltip">Full Screen</div>
